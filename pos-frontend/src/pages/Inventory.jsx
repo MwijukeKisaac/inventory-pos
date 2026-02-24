fd6313850d4233481c20c81fa47474ba5e045432
Mwijukye Kwetegyeka Isaac <138666323+MwijukeKisaac@users.noreply.github.com>
2026-02-24 23:00:50 +0300

    main

--- pos-frontend/src/pages/Inventory.jsx
+++
@@ -1,3 +0,0 @@
-export default function Inventory() {
-  return <h1 className="text-xl font-bold">Inventory Management</h1>;
-}
--- pos-mobile/app/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://YOUR_PC_IP:5000/api", // change later
-  timeout: 5000,
-});
-
-export default api;
